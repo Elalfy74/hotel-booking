6f03a78d19b7c6c6b922d0c1dea2935d2be4876f
elalfy74 <mahmoudelalfy13@gmail.com>
2023-11-17 00:35:56 +0200

    Added Prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button';
+import prisma from '@/lib/prisma';
 
-export default function Home() {
+export default async function Home() {
   return (
     <main className='flex items-center justify-center h-screen w-full'>
       <Button>Shadcn Working</Button>
