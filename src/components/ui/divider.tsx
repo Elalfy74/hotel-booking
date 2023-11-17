39e67cec7da96a4383551fc3da38eb3e79f615cd
elalfy74 <mahmoudelalfy13@gmail.com>
2023-11-17 05:32:23 +0200

    Refactor Divider component styles

--- src/components/ui/divider.tsx
+++ src/components/ui/divider.tsx
@@ -1,9 +1,9 @@
 export const Divider = ({ children }: { children: string }) => {
   return (
-    <div className='relative flex py-5 items-center'>
-      <div className='flex-grow border-t'></div>
-      <span className='flex-shrink mx-4 text-gray-400'>{children}</span>
-      <div className='flex-grow border-t'></div>
+    <div className="relative flex items-center py-5 text-sm">
+      <div className="flex-grow border-t"></div>
+      <span className="mx-4 flex-shrink text-gray-400">{children}</span>
+      <div className="flex-grow border-t"></div>
     </div>
   );
 };
