7f9d138888fc27510f1485190989549cf370b731
elalfy74 <mahmoudelalfy13@gmail.com>
2023-11-17 04:51:09 +0200

    Add login and sign-up content to auth modal

---
+++ src/components/auth/shared/auth-modal-content.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { useEffect } from 'react';
+
+import { LoginModalContent } from '../login/login-modal-content';
+import { SignUpModalContent } from '../sign-up/sign-up-modal-content';
+import { ContentKey } from './auth.modal';
+
+interface AuthModalContentProps {
+  currentContentKey: ContentKey;
+  resetContent: () => void;
+  handleContentChange: (contentKey: ContentKey) => void;
+}
+
+export type ContentComponentProps = {
+  handleContentChange: (contentKey: ContentKey) => void;
+};
+
+type Contents = Record<ContentKey, React.FC<ContentComponentProps>>;
+
+const contents: Contents = {
+  login: LoginModalContent,
+  signUp: SignUpModalContent,
+};
+
+export const AuthModalContent = ({
+  currentContentKey,
+  resetContent,
+  handleContentChange,
+}: AuthModalContentProps) => {
+  const ContentComponent = contents[currentContentKey];
+
+  useEffect(() => {
+    resetContent();
+  }, [resetContent]);
+
+  return <ContentComponent handleContentChange={handleContentChange} />;
+};
