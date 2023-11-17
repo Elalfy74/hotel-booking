bde282bffcb6c29e1d6710c6bae8c62fb6c0d850
elalfy74 <mahmoudelalfy13@gmail.com>
2023-11-17 06:34:27 +0200

    Refactor page placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import prisma from '@/lib/prisma';
 
 export default async function Home() {
   return (
-    <main className='flex items-center justify-center h-screen w-full'>
-      <Button>Shadcn Working</Button>
+    <main className="flex h-screen w-full items-center justify-center">
+      <p>Shadcn Working</p>
     </main>
   );
 }
