0cdc19a17b8d3a7636e04c2c98e4eb4fca32d23f
elalfy74 <mahmoudelalfy13@gmail.com>
2023-11-25 14:07:59 +0200

    Add createUser action to handle user creation

---
+++ src/actions/create-user.ts
@@ -0,0 +1,40 @@
+'use server';
+
+import { Prisma } from '@prisma/client';
+
+import { signUpSchema, SignUpSchemaType } from '@/components/auth/sign-up/sign-up-schema';
+import prisma from '@/lib/prisma';
+
+interface Response<T> {
+  data: null | T;
+  error: null | string;
+}
+
+export const createUser = async (data: SignUpSchemaType) => {
+  const response: Response<null> = {
+    data: null,
+    error: null,
+  };
+
+  try {
+    const isValid = signUpSchema.safeParse(data);
+
+    if (!isValid.success) {
+      response.error = isValid.error.message;
+    }
+
+    await prisma.user.create({ data });
+
+    return response;
+  } catch (err) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+      if (err.code === 'P2002') {
+        response.error = 'Email already exists';
+      }
+    } else if (err instanceof Error) {
+      err.message;
+    }
+
+    return response;
+  }
+};
