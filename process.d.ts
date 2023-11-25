e6b81b4a697c3e37396c46ff115a64be0bd16c71
elalfy74 <mahmoudelalfy13@gmail.com>
2023-11-25 14:07:13 +0200

    Add environment variables for authentication providers

---
+++ process.d.ts
@@ -0,0 +1,16 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    NEXTAUTH_URL: string;
+    NEXTAUTH_SECRET: string;
+    GITHUB_CLIENT_ID: string;
+    GITHUB_CLIENT_SECRET: string;
+    FACEBOOK_ID: string;
+    FACEBOOK_SECRET: string;
+    TWITTER_ID: string;
+    TWITTER_SECRET: string;
+    GOOGLE_ID: string;
+    GOOGLE_SECRET: string;
+    AUTH0_ID: string;
+    AUTH0_SECRET: string;
+  }
+}
