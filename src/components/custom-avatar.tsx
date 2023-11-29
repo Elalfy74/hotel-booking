b211538e14815490266cf98a4422ee6424d17926
elalfy74 <mahmoudelalfy13@gmail.com>
2023-11-29 20:04:36 +0200

    Refactor CustomAvatar component to support children and onLoad callback

--- src/components/custom-avatar.tsx
+++ src/components/custom-avatar.tsx
@@ -7,22 +7,24 @@ import { Avatar, AvatarFallback } from './ui/avatar';
 interface CustomAvatarProps {
   className?: string;
   src?: string | null;
-  fallBack: string;
+  children: React.ReactNode;
+  onLoad?: () => void;
 }
 
-export const CustomAvatar = ({ className, src, fallBack }: CustomAvatarProps) => {
+export const CustomAvatar = ({ className, src, children, onLoad }: CustomAvatarProps) => {
   return (
     <Avatar className={cn('border', className)}>
       {src && (
         <Image
           src={src}
           alt="avatar"
-          className="aspect-square h-full w-full"
+          className="aspect-square h-full w-full object-cover"
           width={32}
           height={32}
+          onLoad={onLoad}
         />
       )}
-      {!src && <AvatarFallback>{fallBack}</AvatarFallback>}
+      {!src && <AvatarFallback>{children}</AvatarFallback>}
     </Avatar>
   );
 };
