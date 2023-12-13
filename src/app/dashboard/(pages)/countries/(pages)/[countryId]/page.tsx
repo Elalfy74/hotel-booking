8dd2cd45a7b3934f1216bb8899f99af9eeeccc2c
elalfy74 <mahmoudelalfy13@gmail.com>
2023-12-13 04:20:51 +0200

    Add EditCountryForm component to allow editing of country details

--- src/app/dashboard/(pages)/countries/(pages)/[countryId]/page.tsx
+++ src/app/dashboard/(pages)/countries/(pages)/[countryId]/page.tsx
@@ -1,3 +1,18 @@
-export default function EditCountry() {
-  return <div>EditCountry</div>;
+import { SingleItemHeading } from '@/app/dashboard/_components/single-item-heading';
+
+import { EditCountry } from './_components/edit-country';
+
+interface SingleCountryPageProps {
+  params: { countryId: string };
+}
+
+export const dynamic = 'force-static';
+
+export default function SingleCountryPage({ params: { countryId } }: SingleCountryPageProps) {
+  return (
+    <>
+      <SingleItemHeading title="countries" singleTile="Country" />
+      <EditCountry countryId={countryId} />
+    </>
+  );
 }
