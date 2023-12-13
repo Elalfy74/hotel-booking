9fa9983db2200a6b3dfb827bd041ff16891ca40b
elalfy74 <mahmoudelalfy13@gmail.com>
2023-12-14 00:21:07 +0200

    Update createCitySchema in create-city.ts

--- src/app/dashboard/(pages)/cities/_schemas/create-city.ts
+++ src/app/dashboard/(pages)/cities/_schemas/create-city.ts
@@ -4,8 +4,8 @@ import { booleanSchema, imageSchema } from '@/lib/utils';
 
 export const createCitySchema = z.object({
   name: z.string().min(2).max(255),
-  images: z.array(imageSchema).nonempty(),
-  countryId: z.string().cuid(),
+  images: z.array(imageSchema).min(1).max(5),
+  countryId: z.string().min(10, 'Invalid Country'),
   isFeatured: booleanSchema,
 });
 export type CreateCityType = z.infer<typeof createCitySchema>;
