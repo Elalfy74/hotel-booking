8e5f7d4d2816d383e9fb074695513f948472f429
elalfy74 <mahmoudelalfy13@gmail.com>
2023-12-15 05:05:51 +0200

    Add getIdColumn and getSelectColumn functions

---
+++ src/components/ui/get-id-column.tsx
@@ -0,0 +1,13 @@
+import { ColumnDef } from '@tanstack/react-table';
+
+import { DataTableColumnHeader } from './data-table-column-header';
+
+export function getIdColumn<T>(): ColumnDef<T> {
+  return {
+    accessorKey: 'id',
+    header: ({ column }) => <DataTableColumnHeader column={column} title="User" />,
+    cell: ({ row }) => <div className="max-w-[80px] truncate">{row.getValue('id')}</div>,
+    enableSorting: false,
+    enableHiding: false,
+  };
+}
