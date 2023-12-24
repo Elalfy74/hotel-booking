fe098a91016ff1d05a1805db8fd95bbc4a2a542f
elalfy74 <mahmoudelalfy13@gmail.com>
2023-12-24 15:41:25 +0200

    Refactor Nav component to use useCallback hook

--- src/app/dashboard/_components/nav.tsx
+++ src/app/dashboard/_components/nav.tsx
@@ -3,6 +3,7 @@
 import { BarChart3Icon, Building2Icon, GlobeIcon, Users2Icon } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useCallback } from 'react';
 
 import { cn } from '@/lib/utils';
 
@@ -20,39 +21,46 @@ const navigation = [
 export const Nav = () => {
   const currentRoute = usePathname();
 
-  const getIsActive = (href: string) => {
-    if (href === '/dashboard') {
-      return currentRoute === href;
-    }
+  const getIsActive = useCallback(
+    (href: string) => {
+      if (href === '/dashboard') {
+        return currentRoute === href;
+      }
 
-    return currentRoute.startsWith(href);
-  };
+      return currentRoute.startsWith(href);
+    },
+    [currentRoute],
+  );
 
   return (
     <nav className="mt-5 flex-1 space-y-1 px-2" aria-label="Sidebar">
-      {navigation.map((item) => (
-        <Link
-          key={item.name}
-          href={item.href}
-          className={cn(
-            'group flex items-center rounded-md px-2 py-2 text-sm font-medium transition-colors',
-            getIsActive(item.href)
-              ? 'bg-primary/5 text-primary hover:bg-primary/5 hover:text-primary'
-              : 'text-muted-foreground hover:bg-muted/50',
-          )}
-        >
-          <>
-            <item.icon
-              className={cn(
-                'mr-3 h-6 w-6 flex-shrink-0',
-                getIsActive(item.href) ? 'text-primary' : 'text-gray-400 ',
-              )}
-              aria-hidden="true"
-            />
-            <span className="flex-1">{item.name}</span>
-          </>
-        </Link>
-      ))}
+      {navigation.map((item) => {
+        const isActive = getIsActive(item.href);
+
+        return (
+          <Link
+            key={item.name}
+            href={item.href}
+            className={cn(
+              'group flex items-center rounded-md px-2 py-2 text-sm font-medium transition-colors',
+              isActive
+                ? 'bg-primary/5 text-primary hover:bg-primary/5 hover:text-primary'
+                : 'text-muted-foreground hover:bg-muted/50',
+            )}
+          >
+            <>
+              <item.icon
+                className={cn(
+                  'mr-3 h-6 w-6 flex-shrink-0',
+                  isActive ? 'text-primary' : 'text-gray-400 ',
+                )}
+                aria-hidden="true"
+              />
+              <span className="flex-1">{item.name}</span>
+            </>
+          </Link>
+        );
+      })}
     </nav>
   );
 };
