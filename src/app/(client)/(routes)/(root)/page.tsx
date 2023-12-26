7f7b2378b87e1fced421581b50f9fb4172075d99
elalfy74 <mahmoudelalfy13@gmail.com>
2023-12-26 22:41:17 +0200

    Add ContactBanner component

--- src/app/(client)/(routes)/(root)/page.tsx
+++ src/app/(client)/(routes)/(root)/page.tsx
@@ -1,3 +1,4 @@
+import { ContactBanner } from '../../_components/contact-banner';
 import {
   ExploreTheWorldSection,
   FeaturedCitiesSection,
@@ -21,6 +22,7 @@ export default async function Home() {
         <TopToursSection />
         <ExploreTheWorldSection />
         <TrendingHotelsSection />
+        <ContactBanner />
       </div>
     </main>
   );
---
+++ src/app/(client)/_components/contact-banner.tsx
@@ -0,0 +1,25 @@
+import { Button } from '@/components/ui/button';
+
+export const ContactBanner = () => {
+  return (
+    <section className="section">
+      <div className="flex flex-wrap justify-between gap-6 rounded-xl bg-primary p-4 md:p-12">
+        <div className="text-white">
+          <h2 className="mb-3 text-2xl font-bold text-slate-100 md:text-4xl">Get our pro offers</h2>
+          <p>
+            Create visual identify for your company
+            <br /> and an overall brand
+          </p>
+        </div>
+        <div className="flex  w-full justify-between self-end rounded-xl bg-white p-2 md:gap-10 lg:w-auto lg:justify-start">
+          <input
+            type="email"
+            className="w-full px-3 text-sm outline-none lg:text-base"
+            placeholder="Type your email here"
+          />
+          <Button className="md:px-5 md:py-2.5">Subscribe</Button>
+        </div>
+      </div>
+    </section>
+  );
+};
