71e2cc1c21da5c85adf9e94a7f416d8e701b6fc2
elalfy74 <mahmoudelalfy13@gmail.com>
2023-12-27 19:26:46 +0200

    Refactor import statements in page.tsx

--- src/app/(client)/(routes)/(root)/page.tsx
+++ src/app/(client)/(routes)/(root)/page.tsx
@@ -1,24 +1,12 @@
-import dynamic from 'next/dynamic';
-
-import { FeaturedDestinationsSection } from './_components/sections';
-import { FeaturedCitiesSection } from './_components/sections/featured-cities-section/featured-cities-section';
-import { HeroSection } from './_components/sections/hero-section/hero-section';
-
-const TopToursSection = dynamic(
-  () => import('./_components/sections').then((mod) => mod.TopToursSection),
-  {
-    ssr: false,
-  },
-);
-const ExploreTheWorldSection = dynamic(() =>
-  import('./_components/sections').then((mod) => mod.ExploreTheWorldSection),
-);
-const TrendingHotelsSection = dynamic(() =>
-  import('./_components/sections').then((mod) => mod.TrendingHotelsSection),
-);
-const ContactBanner = dynamic(() =>
-  import('../../_components/contact-banner').then((mod) => mod.ContactBanner),
-);
+import { ContactBanner } from '../../_components/contact-banner';
+import {
+  ExploreTheWorldSection,
+  FeaturedCitiesSection,
+  FeaturedDestinationsSection,
+  HeroSection,
+  TopToursSection,
+  TrendingHotelsSection,
+} from './_components/sections';
 
 export default async function Home() {
   return (
