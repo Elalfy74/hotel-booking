494f684aca13bf1229d95276337a2a8cebff725e
elalfy74 <mahmoudelalfy13@gmail.com>
2023-12-27 21:04:48 +0200

    fix lazy component

--- src/components/lazy-component.tsx
+++ src/components/lazy-component.tsx
@@ -1,7 +1,15 @@
+import { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 export const LazyComponent = ({ children }: { children: React.ReactNode }) => {
   const { ref, inView } = useInView();
+  const [shouldRender, setShouldRender] = useState(false);
 
-  return <div ref={ref}>{inView && children}</div>;
+  useEffect(() => {
+    if (inView) {
+      setShouldRender(true);
+    }
+  }, [inView]);
+
+  return <div ref={ref}>{shouldRender && children}</div>;
 };
