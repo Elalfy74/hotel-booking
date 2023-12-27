e22252902b6ee2cd3f96694e4d3b38e6affd85ed
elalfy74 <mahmoudelalfy13@gmail.com>
2023-12-27 15:40:34 +0200

    Refactor featured destinations section

--- src/app/(client)/(routes)/(root)/_components/sections/featured-destinations-section/featured-destinations-section.tsx
+++ src/app/(client)/(routes)/(root)/_components/sections/featured-destinations-section/featured-destinations-section.tsx
@@ -1,8 +1,6 @@
 import prisma from '@/lib/prisma';
 
-import { SectionHeading } from '../../section-heading';
-import { Backdrop } from './backdrop';
-import { DestinationView } from './destination-view';
+import { LazyFeaturedDestinations } from './lazy-feature-destinations';
 
 const getFeaturedDestinations = async () => {
   return prisma.city.findMany({
@@ -20,15 +18,5 @@ export type IFeaturedDestination = AwaitedReturn<typeof getFeaturedDestinations>
 export const FeaturedDestinationsSection = async () => {
   const featuredDestinations = await getFeaturedDestinations();
 
-  return (
-    <section className="section">
-      <SectionHeading title="Featured Destinations" desc="Popular destinations open to visitors" />
-      <div className="relative grid h-auto grid-cols-1 gap-6 md:h-[600px] md:grid-cols-3 md:grid-rows-9">
-        {featuredDestinations.map((destination) => (
-          <DestinationView key={destination.id} destination={destination} />
-        ))}
-      </div>
-      <Backdrop />
-    </section>
-  );
+  return <LazyFeaturedDestinations destinations={featuredDestinations} />;
 };
